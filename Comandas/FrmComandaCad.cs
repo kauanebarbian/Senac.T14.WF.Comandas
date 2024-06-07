8b2da9def4307d5e417c45b6613535908127f6e2
kauanebarbian <barbiankauane@gmail.com>
2024-06-06 21:13:01 -0300

    sdsdsds

--- Comandas/FrmComandaCad.cs
+++ Comandas/FrmComandaCad.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
+using System.Diagnostics.Contracts;
 using System.Drawing;
 using System.Linq;
 using System.Text;
@@ -15,6 +16,28 @@ namespace Comandas
         public FrmComandaCad()
         {
             InitializeComponent();
+        }
+
+        //evento de clique
+        private void btnCardapio_Click(object sender, EventArgs e)
+        {
+            //cria o formulario e exibe
+            new FrmCardapio().ShowDialog();
+        }
+
+        private void btnComanda_Click_1(object sender, EventArgs e)
+        {
+            new FrmComanda().ShowDialog();
+        }
+
+        private void btnPedidoCozinha_Click(object sender, EventArgs e)
+        {
+            new FrmPedidoCozinha().ShowDialog();
+        }
+
+        private void btnUsuarios_Click(object sender, EventArgs e)
+        {
+            
         }
     }
 }
--- Comandas/FrmPrincipal.Designer.cs
+++ Comandas/FrmPrincipal.Designer.cs
@@ -28,12 +28,84 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
-            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            airButton1 = new ReaLTaiizor.Controls.AirButton();
+            airButton2 = new ReaLTaiizor.Controls.AirButton();
+            airButton3 = new ReaLTaiizor.Controls.AirButton();
+            airButton4 = new ReaLTaiizor.Controls.AirButton();
+            SuspendLayout();
+            // 
+            // airButton1
+            // 
+            airButton1.Customization = "7e3t//Ly8v/r6+v/5ubm/+vr6//f39//p6en/zw8PP8UFBT/gICA/w==";
+            airButton1.Font = new Font("Segoe UI", 9F);
+            airButton1.Image = null;
+            airButton1.Location = new Point(54, 146);
+            airButton1.Name = "airButton1";
+            airButton1.NoRounding = false;
+            airButton1.Size = new Size(150, 129);
+            airButton1.TabIndex = 0;
+            airButton1.Text = "airButton1";
+            airButton1.Transparent = false;
+            // 
+            // airButton2
+            // 
+            airButton2.Customization = "7e3t//Ly8v/r6+v/5ubm/+vr6//f39//p6en/zw8PP8UFBT/gICA/w==";
+            airButton2.Font = new Font("Segoe UI", 9F);
+            airButton2.Image = null;
+            airButton2.Location = new Point(231, 146);
+            airButton2.Name = "airButton2";
+            airButton2.NoRounding = false;
+            airButton2.Size = new Size(150, 129);
+            airButton2.TabIndex = 1;
+            airButton2.Text = "airButton2";
+            airButton2.Transparent = false;
+            // 
+            // airButton3
+            // 
+            airButton3.Customization = "7e3t//Ly8v/r6+v/5ubm/+vr6//f39//p6en/zw8PP8UFBT/gICA/w==";
+            airButton3.Font = new Font("Segoe UI", 9F);
+            airButton3.Image = null;
+            airButton3.Location = new Point(422, 146);
+            airButton3.Name = "airButton3";
+            airButton3.NoRounding = false;
+            airButton3.Size = new Size(150, 129);
+            airButton3.TabIndex = 2;
+            airButton3.Text = "airButton3";
+            airButton3.Transparent = false;
+            // 
+            // airButton4
+            // 
+            airButton4.Customization = "7e3t//Ly8v/r6+v/5ubm/+vr6//f39//p6en/zw8PP8UFBT/gICA/w==";
+            airButton4.Font = new Font("Segoe UI", 9F);
+            airButton4.Image = null;
+            airButton4.Location = new Point(611, 146);
+            airButton4.Name = "airButton4";
+            airButton4.NoRounding = false;
+            airButton4.Size = new Size(150, 129);
+            airButton4.TabIndex = 3;
+            airButton4.Text = "airButton4";
+            airButton4.Transparent = false;
+            // 
+            // FrmPrincipal
+            // 
+            AutoScaleDimensions = new SizeF(10F, 25F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(800, 450);
+            Controls.Add(airButton4);
+            Controls.Add(airButton3);
+            Controls.Add(airButton2);
+            Controls.Add(airButton1);
+            Name = "FrmPrincipal";
+            Text = "Form1";
+            ResumeLayout(false);
         }
 
         #endregion
+
+
+        private ReaLTaiizor.Controls.AirButton airButton1;
+        private ReaLTaiizor.Controls.AirButton airButton2;
+        private ReaLTaiizor.Controls.AirButton airButton3;
+        private ReaLTaiizor.Controls.AirButton airButton4;
     }
 }
